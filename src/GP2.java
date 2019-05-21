7f5e596fe4870464123341af8ac62785535e605b
Quplet <jonathanhart1114@gmail.com>
2019-05-21 12:04:50 -0400

    Bug fixes

--- src/GP2.java
+++ src/GP2.java
@@ -1,16 +1,20 @@
 public class GP2 
 {
-	int a;
-	int b; 
+	//Changed the fields to private
+	private int a;
+	private int b; 
+
 	public GP2( int a, int b ) 
 	{
 		this.a = a; 
 		this.b = b;
 		generateNum();
 	}
+
 	public int generateNum() 
 	{
-		int c = (int)(Math.random()*(b-a));
+		//Made the random inclusive to the upper bound
+		int c = (int)(Math.random()*(b-a+1));
 		return a + c;
 	}
 }
